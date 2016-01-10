8927668ced03a0189b1633b21f08f394139e7bf9
PythEch <pythech.tr@gmail.com>
2016-01-10 22:46:47 +0200

    oops

--- main.c
+++ main.c
@@ -10,17 +10,17 @@ char *GET_BALANCE_URL;
 void init() {
   KRIST_SYNC_URL = httpGet("https://raw.githubusercontent.com/BTCTaras/"
                            "kristwallet/master/staticapi/syncNode");
-  KRIST_SYNC_URL[strlen(KRIST_SYNC_URL) - 2] = '\0';
+  KRIST_SYNC_URL[strlen(KRIST_SYNC_URL) - 1] = '\0';
 
-  LAST_BLOCK_URL = malloc(strlen(KRIST_SYNC_URL) + strlen("/?lastblock") + 1);
-  sprintf(LAST_BLOCK_URL, "%s/?lastblock", KRIST_SYNC_URL);
+  LAST_BLOCK_URL = malloc(strlen(KRIST_SYNC_URL) + strlen("?lastblock") + 1);
+  sprintf(LAST_BLOCK_URL, "%s?lastblock", KRIST_SYNC_URL);
 
-  GET_WORK_URL = malloc(strlen(KRIST_SYNC_URL) + strlen("/?getwork") + 1);
-  sprintf(GET_WORK_URL, "%s/?getwork", KRIST_SYNC_URL);
+  GET_WORK_URL = malloc(strlen(KRIST_SYNC_URL) + strlen("?getwork") + 1);
+  sprintf(GET_WORK_URL, "%s?getwork", KRIST_SYNC_URL);
 
   GET_BALANCE_URL =
-      malloc(strlen(KRIST_SYNC_URL) + strlen("/?getbalance=") + 1);
-  sprintf(GET_BALANCE_URL, "%s/?getbalance=", KRIST_SYNC_URL);
+      malloc(strlen(KRIST_SYNC_URL) + strlen("?getbalance=") + 1);
+  sprintf(GET_BALANCE_URL, "%s?getbalance=", KRIST_SYNC_URL);
 }
 
 int main(int argc, char **argv) {
